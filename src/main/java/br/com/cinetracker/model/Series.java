30fdac148ba307da4f0fa59b774284a5228b9b12
Vitor Staub <vitorhugostaub@hotmail.com>
2024-06-21 15:19:43 -0300

    feat: Series class with individuals attributes

---
+++ src/main/java/br/com/cinetracker/model/Series.java
@@ -0,0 +1,64 @@
+package br.com.cinetracker.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+
+import java.util.OptionalDouble;
+
+public class Series {
+    private String title;
+    private Integer totalSeasons;
+    private Double rating;
+    private Category genre;
+    private String actors;
+    private String poster;
+    private String plot;
+
+    public Series(SeriesData d) {
+        this.title = d.title();
+        this.totalSeasons = d.totalSeasons();
+        this.rating = OptionalDouble.of(Double.parseDouble(d.rating())).orElse(0);
+        this.genre = Category.fromString(d.genre().split(",")[0].strip());
+        this.actors = d.actors();
+        this.poster = d.poster();
+        this.plot = d.plot();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Integer getTotalSeasons() {
+        return totalSeasons;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public Category getGenre() {
+        return genre;
+    }
+
+    public String getActors() {
+        return actors;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public String getPlot() {
+        return plot;
+    }
+
+    @Override
+    public String toString() {
+        return "title=" + title +
+                " totalSeasons=" + totalSeasons +
+                " rating=" + rating +
+                " genre=" + genre +
+                " actors=" + actors +
+                " poster=" + poster +
+                " plot=" + plot;
+    }
+}
