e43940ee48f63c9212e7d559e7e3bd9abeb6b873
Vitor Staub <vitorhugostaub@hotmail.com>
2024-06-21 13:54:49 -0300

    feat: expand SeriesData with additional fields

--- src/main/java/br/com/cinetracker/model/SeriesData.java
+++ src/main/java/br/com/cinetracker/model/SeriesData.java
@@ -6,7 +6,11 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record SeriesData(@JsonAlias("Title") String title,
                          @JsonAlias("totalSeasons") Integer totalSeasons,
-                         @JsonAlias("imdbRating") String rating) {
+                         @JsonAlias("imdbRating") String rating,
+                         @JsonAlias("Genre") String genre,
+                         @JsonAlias("Actors") String actors,
+                         @JsonAlias("Poster") String poster,
+                         @JsonAlias("Plot") String plot){
     public SeriesData {
         if (title == null || totalSeasons == null || rating == null) {
             throw new NullPointerException("Series data is null!");
